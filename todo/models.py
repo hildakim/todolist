ed0d8c9fb925c430d30ff2a87b42f6ef822c944e
hildakim <pinkredjohn@gmail.com>
2021-08-18 00:45:20 +0900

    todo:base

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields.related import ManyToManyField
 
 # Create your models here.
+class Todo(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    isCompleted = models.BooleanField(default=False)
+    like = ManyToManyField(get_user_model(), related_name='like', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    todo_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created']
