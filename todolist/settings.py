a07e11c7ff67494d95696dd41fb842e581ad0b8a
hildakim <pinkredjohn@gmail.com>
2021-08-20 01:14:53 +0900

    release ready

--- todolist/settings.py
+++ todolist/settings.py
@@ -20,16 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5a1_k7+l3f-6jzm-zntxl@2o8!!%@+b*+7f%&qr+u4q5ze7o#1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-5a1_k7+l3f-6jzm-zntxl@2o8!!%@+b*+7f%&qr+u4q5ze7o#1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
-
-# DEBUG =  bool( os.environ.get('DJANGO_DEBUG', True) )
-
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 
@@ -64,7 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -185,9 +181,9 @@ DATE_INPUT_FORMATS = ['%Y-%m-%d']
 TIME_INPUT_FORMATS = ['%H:%M']
 
 # Heroku: Update database configuration from $DATABASE_URL. 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500) 
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
